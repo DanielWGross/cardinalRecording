57b134c30d83819a2c2b238eff5c7e86a8c9997b
mccaffertycr <mccaffertycr@gmail.com>
2018-08-23 20:41:35 -0400

    changes to client and equipment models

--- models/plugin.js
+++ models/plugin.js
@@ -1,11 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
   var Plugin = sequelize.define('Plugin', {
-    effects_packages: DataTypes.STRING,
-    waves: DataTypes.STRING,
-    slate_digital: DataTypes.STRING,
-    fab_filter: DataTypes.STRING,
-    izotope: DataTypes.STRING,
-    soundtoys: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    effects_packages: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    waves: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    slate_digital: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    fab_filter: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    izotope: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    soundtoys: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
   });
 
   Plugin.associate = (models) => {
