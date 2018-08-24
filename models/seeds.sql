a5d930f9ac9d9913ad5563a1231c6b06b40fcad8
mccaffertycr <mccaffertycr@gmail.com>
2018-08-24 11:29:20 -0400

    equipment seeds sans plugins, and a dummy client

---
+++ models/seeds.sql
@@ -0,0 +1,161 @@
+-- Interfaces/Mic Pres/Monitoring Speakers
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Focusrite: Scarlett 18i20 (X2)', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('SSL: Alpha VHD analog four channel', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Universal Audio: Apollo Twin', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Yamaha: HS8 Monitoring Speakers', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Mackie: HR824 Monitoring Speakers', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Rupert Neve: Portico 5012', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('AKG: K240 Studio Headphones', 1);
+
+INSERT INTO Equipment (name, interfaces)
+VALUES ('Samson: SR950 Studio Headphones', 1);
+
+-- Microphones
+
+INSERT INTO Equipment (name, microphones)
+VALUES('AKG: C214 x2, AKG D112, Perception 200 x2', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Shure: Beta 52a, SM57, SM7b, SM81', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Audix: i5', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Sennheiser: E602,  e902,  MD 421 II', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Audio Technica: 3035', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Rode: M5 (matched pair)', 1);
+
+INSERT INTO Equipment (name, microphones)
+VALUES ('Neumann: TLM 103', 1);
+
+-- cables
+
+INSERT INTO Equipment (name, cables)
+VALUES('Monster: XLR and 1/4"', 1);
+
+INSERT INTO Equipment (name, cables)
+VALUES ('Mogami: XLR', 1);
+
+-- Instruments
+
+INSERT INTO Equipment (name, instruments)
+VALUES('Gibson Les Paul Studio', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('PRS Custom 24', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Taylor Acoustic', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Gibson Grabber Bass', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Fender Jazz Bass Special', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Fender P-Bass', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Pearl Export Drum Set', 1);
+
+INSERT INTO Equipment (name, instruments)
+VALUES ('Novation Launchkey 49 Midi Keyboard', 1);
+
+-- Amps/Cabs
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Marshall: JCM 2000', 1);
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Orange: Rockerverb 50', 1);
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Peavey: 5150', 1);
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Marshall: 4x12 w/ Celestian Greenback 1960 Cab', 1);
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Mesa: Recto Cab 4x12', 1); 
+INSERT INTO Equipment (name, amps)
+VALUES ('Fender: Modified 1974 Delux Twin Reverb', 1);
+
+INSERT INTO Equipment (name, amps)
+VALUES ('Kemper Profiling Amp', 1); 
+
+-- Pedals
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Sans amp: D.I.', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Electro Harmonix: Pi Big Muff (bass)', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Electro Harmonix: Big Muff (guitar)', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Dark Glass: BK7 Micro Tube', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Ibanez: Tube Screamer', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Ratt: Distortion', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('T-Rex: Delay', 1); 
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Fulltone: OCD', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Boss: OC-3', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('MXR-288: Bass Octave', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Korg Rack Tuner', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Joyo: Vintage Overdrive', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Joyo: Delux Distortion', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Joyo: Delux Crunch', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('T.C. Electronics: Rottweiler', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Classic Wah', 1);
+
+INSERT INTO Equipment (name, pedals)
+VALUES ('Ebow', 1);
+
+
+
+
