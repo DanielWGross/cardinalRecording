dbd7d67b7d292e9ede3c05552e1dfacb51fa0798
Daniel Gross <danwgross@gmail.com>
2018-08-26 15:44:21 -0400

    route clean up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,84 +1,78 @@
-var db = require('../models');
+const db = require('../models');
 
 module.exports = (app) => {
-  // get all equipment
+  // GET All Equipment
   app.get('/api/equipment', (req, res) => {
     db.Equipment.findAll({
-      include: [ models.Plugin ]
-    }).then((equipment) => {
-      res.json(equipment);
-    })
-  })
-  
-  // get all clients
-  app.get('/api/clients', (req, res) => {
-    db.Client.findAll({}).then((clients) => {
-      res.json(clients);
+    }).then((dbEquipment) => {
+      res.json(dbEquipment);
     });
   });
-
-  //
-  //
-  // admin 
-  //
-  //
-  // just a preliminary skeleton, our admin functionality will live here and in a client side js file with the event listeners
+  // GET All Clients
+  app.get('/api/clients', (req, res) => {
+    db.Client.findAll({})
+    .then((dbClients) => {
+      res.json(dbClients);
+    });
+  });
+  // POST Contact Form
   app.post('/contact', (req, res) => {
-    db.Contact.create(req.body).then((dbContact) => {
+    db.Contact.create(req.body)
+    .then((dbContact) => {
       res.json(dbContact);
     });
   });
-
-  app.post('/equipment/admin', (req, res) => {
-    db.Equipment.create(req.body).then((equipment) => {
-      res.json(equipment); // return result and append html on client side via jQuery
-                           // would be nice if this was a preview where the changes could be undone or edited 
-    });
-  })
-
-  app.put('/equipment/admin', (req, res) => {
-    db.Equipment.update({
-      x: y,
-      where: {
-        id: req.body.id
-      }
-    }).then((equipment) => {
-      res.json(equipment); // "
-    });
-  })
-
-  app.delete('/equipment/admin', (req, res) => {
-    db.Equipment.destroy({
-      where: {
-        x: y
-      }
-    }).then((equipment) => {
-      res.json(equipment); // "
-    });
-  });
-
-  app.post('/clients/admin', (req, res) => {
-    db.Client.create(req.body).then((dbExample) => {
-      res.json(clients); 
-    });
-  });
-
-  app.put('/clients/admin', (req, res) => {
-    db.Client.update({
-      x: y,
-      where: {
-        id: req.body.id
-      }
-    });
-  });
-
-  app.delete('/clients/admin', (req, res) => {
-    db.Client.destroy({ 
-      where: { 
-        id: req.body.id 
-      } 
-    }).then((clients) => {
-      res.json(clients);
-    });
-  });
 };
+// ---------------------------------------------------FUTURE ADMIN ROUTES------------------------------------------------------------------
+  // app.post('/equipment/admin', (req, res) => {
+  //   db.Equipment.create(req.body).then((equipment) => {
+  //     res.json(equipment); // return result and append html on client side via jQuery
+  //                          // would be nice if this was a preview where the changes could be undone or edited 
+  //   });
+  // })
+
+//   app.put('/equipment/admin', (req, res) => {
+//     db.Equipment.update({
+//       x: y,
+//       where: {
+//         id: req.body.id
+//       }
+//     }).then((equipment) => {
+//       res.json(equipment); // "
+//     });
+//   })
+
+//   app.delete('/equipment/admin', (req, res) => {
+//     db.Equipment.destroy({
+//       where: {
+//         x: y
+//       }
+//     }).then((equipment) => {
+//       res.json(equipment); // "
+//     });
+//   });
+
+//   app.post('/clients/admin', (req, res) => {
+//     db.Client.create(req.body).then((dbExample) => {
+//       res.json(clients); 
+//     });
+//   });
+
+//   app.put('/clients/admin', (req, res) => {
+//     db.Client.update({
+//       x: y,
+//       where: {
+//         id: req.body.id
+//       }
+//     });
+//   });
+
+//   app.delete('/clients/admin', (req, res) => {
+//     db.Client.destroy({ 
+//       where: { 
+//         id: req.body.id 
+//       } 
+//     }).then((clients) => {
+//       res.json(clients);
+//     });
+//   });
