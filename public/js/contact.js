47a612f4659de0fcf248250a66e8a55f40be7d2f
Daniel Gross <danwgross@gmail.com>
2018-08-26 14:56:40 -0400

    contact.js refactor

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,32 +1,36 @@
-// Event Handler for Submit Button
-// Grab submit button.
+// Get references to page elements
 const submitButton = document.querySelector('.main-btn');
-// Add click event handler to submit button
-submitButton.addEventListener('click', (event) => {
+// The API object contains methods for each kind of request we will make.
+const API = {
+  postAPI: function(data) {
+    return fetch("/", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }
+};
+// Logic to handle the click event on the submit button
+const handleSubmit = (event) => {
   event.preventDefault();
-  // Create new object with the values from the form
+
   const newContact = {
     name: document.querySelector('#name').value.trim(),
     email: document.querySelector('#email').value.trim(),
     subject: document.querySelector('#subject').value.trim(),
     message: document.querySelector('#message').value.trim(),
   };
-  // POST fetch call passing newContact object
-  fetch('/', {
-    method: "POST",
-    // Set headers for JSON
-    headers: 
-    {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(newContact)
-    // Once complete...
-  }).then(response => {
-    // Reset values of contact form
+
+  API.postAPI(newContact)
+  .then(res => {
     document.querySelector('#name').value = "";
     document.querySelector('#email').value = "";
     document.querySelector('#subject').value = "";
     document.querySelector('#message').value = "";
   });
-});
+};
+// Event handlers
+submitButton.addEventListener("click", handleSubmit);
