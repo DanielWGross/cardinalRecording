300525d55845bafcb2e8e7d03657384c123aad80
mccaffertycr <mccaffertycr@gmail.com>
2018-08-27 20:16:27 -0400

    vanilla mailer, needs to be connected with contact page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const nodemailer = require('../controllers/mailer');
 
 module.exports = (app) => {
   // GET All Equipment
@@ -17,9 +18,9 @@ module.exports = (app) => {
   });
   // POST Contact Form
   app.post('/contact', (req, res) => {
-    db.Contact.create(req.body)
+    console.log(req.body)
     .then((dbContact) => {
-      res.json(dbContact);
+      // res.json(dbContact);
     });
   });
 };
